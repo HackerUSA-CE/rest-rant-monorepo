cec0cfb44f043a828cace8769c8ff140c6fe3813
Joshua Miles <joshua.m@hackerusa.com>
2021-07-27 00:32:40 -0500

    in progress

--- backend/middleware/defineCurrentUser.js
+++ backend/middleware/defineCurrentUser.js
@@ -1,6 +1,15 @@
+const db = require("../models")
 
+const { User } = db;
 
-async function defineCurrentUser(){
-
+async function defineCurrentUser(req, res, next) {
+    let user = await User.findOne({
+        where: {
+            userId: req.session.userId
+        }
+    })
+    req.currentUser = user
+    next()
 }
 
+module.exports = defineCurrentUser
