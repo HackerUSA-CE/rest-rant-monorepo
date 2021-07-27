d34ac4c6c06f4e18df99c8727e6d1cc94b4de5ea
Joshua Miles <joshua.m@hackerusa.com>
2021-07-27 10:03:48 -0500

    adding login form to starter code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Error404 from './Error404'
 import NewPlaceForm from './places/NewPlaceForm'
 import EditPlaceForm from './places/EditPlaceForm'
 import SignUpForm from './users/SignUpForm'
+import LoginForm from './users/Login'
 import CurrentUserProvider from './contexts/CurrentUser'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/sign-up" component={SignUpForm} />
+          <Route exact path="/login" component={LoginForm} />
           <Route exact path="/places" component={PlaceIndex} />
           <Route exact path="/places/new" component={NewPlaceForm} />
           <Route exact path="/places/:placeId" component={PlaceDetails} />
