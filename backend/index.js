e0f83ada6a303a4b5ecf818cfde5c41ad7feec24
Joshua Miles <joshua.m@hackerusa.com>
2021-07-27 00:34:02 -0500

    in progress

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
 const cookieSession = require('cookie-session')
+const defineCurrentUser = require('./middleware/defineCurrentUser')
 
 // Express Settings
 app.use(cookieSession({
@@ -21,6 +22,7 @@ app.use(cors({
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(defineCurrentUser)
 
 // Controllers & Routes
 
