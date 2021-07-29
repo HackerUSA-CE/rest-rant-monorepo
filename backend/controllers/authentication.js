7301b5a4a5c587d507aaffb0c6137884ec3dc007
Joshua Miles <isaacsmiles@gmail.com>
2021-07-29 16:27:45 -0500

    -

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -17,4 +17,17 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
 module.exports = router
