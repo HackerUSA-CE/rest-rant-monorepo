5f3fc84845df690f0cb2403b153350e4f26aefa9
Joshua Miles <isaacsmiles@gmail.com>
2021-07-29 16:08:45 -0500

    -

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -13,6 +13,7 @@ router.post('/', async (req, res) => {
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({ message: `Could not find a user with the provided username and password` })
     } else {
+        req.session.userId = user.userId
         res.json({ user })
     }
 })
