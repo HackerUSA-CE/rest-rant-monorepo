f01848a0104d47dc91c8ea6019444a5bd9b5b1ce
Joshua Miles <isaacsmiles@gmail.com>
2021-07-30 15:18:48 -0500

    Revert "-"
    This reverts commit 7bff8577a0b60b825e393265ad5ac2af3b58ca4e.

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -8,7 +8,6 @@ router.post('/', async (req, res) => {
     let { password, ...rest } = req.body;
     const user = await User.create({ 
         ...rest, 
-        role: 'reviewer',
         passwordDigest: await bcrypt.hash(password, 10)
     })
     res.json(user)
