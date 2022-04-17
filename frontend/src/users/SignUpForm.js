e970999e978439aadfc23f078370bfa2a75fb8f5
Vinh Pham <phamv1@unlv.nevada.edu>
2022-04-17 16:01:15 -0700

    user login and authentication

--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -54,20 +54,35 @@ function SignUpForm() {
 						/>
 					</div>
 				</div>
-				<div className="row">
-					<div className="col-sm-6 form-group">
-						<label htmlFor="email">Email</label>
-						<input
-							type="email"
-							required
-							value={user.email}
-							onChange={e => setUser({ ...user, email: e.target.value })}
-							className="form-control"
-							id="email"
-							name="email"
-						/>
+				  
+					<div className="row">
+						<div className="col-sm-6 form-group">
+							<label htmlFor="email">Email</label>
+							<input
+								type="email"
+								required
+								value={user.email}
+								onChange={e => setUser({ ...user, email: e.target.value })}
+								className="form-control"
+								id="email"
+								name="email"
+							/>
+						</div>
+						<div className="col-sm-6 form-group">
+							<label htmlFor="password">Password</label>
+							<input
+								type="password"
+								required
+								value={user.password}
+								onChange={e => setUser({ ...user, password: e.target.value })}
+								className="form-control"
+								id="password"
+								name="password"
+							/>
+						</div>
 					</div>
-				</div>
+  
+
 				<input className="btn btn-primary" type="submit" value="Sign Up" />
 			</form>
 		</main>
