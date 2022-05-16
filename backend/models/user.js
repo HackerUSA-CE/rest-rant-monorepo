2cd06e3d487767bc9c28ab8fec63a3dc3c49de89
Don Reeves <donjreeves@gmail.com>
2022-05-16 18:46:23 -0400

    backend additions for sessions

--- backend/models/user.js
+++ backend/models/user.js
@@ -19,7 +19,8 @@ module.exports = (sequelize, DataTypes) => {
     },
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    email: DataTypes.STRING
+    email: DataTypes.STRING,
+    passwordDigest: DataTypes.STRING,
   }, {
     sequelize,
     underscored: true,
