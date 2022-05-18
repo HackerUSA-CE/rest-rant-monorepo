c7a589a97622f5c2f3aece3540cbf13e24b08416
Kaden Hansen <kadendeanhansen@gmail.com>
2022-05-18 17:10:55 -0400

    add user login verification logic

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -5,7 +5,19 @@ const bcrypt = require('bcrypt')
 const { User } = db
 
 router.post('/', async (req, res) => {
-    console.log('IN HERE')
+    let user = await User.findOne({
+        where: { email: req.body.email }
+    })
+
+    // console.log(user)
+
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({
+            message: 'Could not find a user with the provided username and password'
+        })
+    } else {
+        res.json({ user })
+    }
 })
 
 module.exports = router
