a7ab10b3454f56618619d1d30bfb7f45c937a619
Kaden Hansen <kadendeanhansen@gmail.com>
2022-05-17 20:52:58 -0400

    add password section to sign up form

--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -68,6 +68,18 @@ function SignUpForm() {
 						/>
 					</div>
 				</div>
+				<div className="col-sm-6 form-group">
+					<label htmlFor="password">Password</label>
+					<input 
+						type="password"
+						required
+						value={user.password}
+						onChange={e => setUser({ ...user, password: e.target.value})}
+						className="form-control"
+						id="password"
+						name="password"
+					/>
+				</div>
 				<input className="btn btn-primary" type="submit" value="Sign Up" />
 			</form>
 		</main>
