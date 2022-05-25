52c898019239284a04da740751b532deb11fae73
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-05-24 22:29:34 -0500

    commit

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
+app.use('/authenticate', require('./controllers/authenticate'))
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -15,9 +15,25 @@ function LoginForm() {
 
     const [errorMessage, setErrorMessage] = useState(null)
 
+
     async function handleSubmit(e) {
         e.preventDefault()
-       
+        const response = await fetch(`http://localhost:5000/authenticate/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+
+        const data = await response.json()
+
+        if (response.status === 200) {
+            setCurrentUser(data.user)
+            history.push(`/`)
+        } else {
+            setErrorMessage(data.message)
+        }
 
     }
 
