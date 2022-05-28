3ebe0655b69a90dcb10e3c8209e58791eae96cbf
Kaden Hansen <kadendeanhansen@gmail.com>
2022-05-28 12:24:19 -0400

    add jwt authentication logic

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require('../models')
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -9,14 +10,34 @@ router.post('/', async (req, res) => {
         where: { email: req.body.email }
     })
 
-    // console.log(user)
-
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({
             message: 'Could not find a user with the provided username and password'
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_Secret, { id: user.userId })
+        res.json({ user: user, token: result.value })
+    }
+})
+
+router.get('/profile', async(req, res) => {
+    try {
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        // handle specific authorization 'Bearer'
+        if (authenticationMethod == 'Bearer'){
+            //decode and retrieve id
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+            const { id } = result.value
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+            res.json(user)
+        }
+    } catch {
+        res.json(null)
     }
 })
 
