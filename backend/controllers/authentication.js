f9e2dca9cb4db647d5e29bd165a3372b8eb1d194
root <mtrhoades0727@gmail.com>
2022-06-14 09:50:00 -1000

    sign up and login

---
+++ backend/controllers/authentication.js
@@ -0,0 +1,36 @@
+
+const router = require('express').Router()
+const db = require("../models")
+const bcrypt = require('bcrypt')
+
+const { User } = db
+
+router.post('/', async (req, res) => {
+
+    let user = await User.findOne({
+        where: { email: req.body.email }
+    })
+
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({ message: `Could not find a user with the provided username and password` })
+    } else {
+        req.session.userId = user.userId
+        res.json({ user })
+    }
+})
+
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
+
+module.exports = router
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,24 @@
 // Modules and Globals
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+const cookieSession = require('cookie-session');
 
 // Express Settings
-app.use(cors())
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(cookieSession({
+    name: 'session',
+    keys: [process.env.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
+app.use(cors({
+    origin: 'http//localhost:3000',
+    credentials: true
+}));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Controllers & Routes
 
@@ -17,6 +26,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
