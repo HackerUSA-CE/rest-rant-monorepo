e117d7a66ef30c0e4c5d91b7f48e955c2927a5dd
Michael Champlin <mchamplin@sbcglobal.net>
2022-06-23 18:50:23 -0700

    Added password to form and front/backend to encrypt and send to DB

--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -68,6 +68,20 @@ function SignUpForm() {
 						/>
 					</div>
 				</div>
+				<div className="row">
+					<div className="col-sm-6 form-group">
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							required
+							value={user.password}
+							onChange={e => setUser({ ...user, password: e.target.value })}
+							className="form-control"
+							id="password"
+							name="password"
+						/>
+					</div>
+				</div>
 				<input className="btn btn-primary" type="submit" value="Sign Up" />
 			</form>
 		</main>
