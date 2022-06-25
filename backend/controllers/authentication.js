3cdbbb4289791b65c48cef75aa0d391adb7e28bb
Ron Owens <rowens72@yahoo.com>
2022-06-25 09:05:37 -0700

    added request handler to the authentication controller not working yet

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -5,18 +5,34 @@ const bcrypt = require('bcrypt')
 const { User } = db
 
 router.post('/', async (req, res) => {
-    let user = await User.findOne({ 
-        where: { email: req.body.email },
+    
+    let user = await User.findOne({
+        where: { email: req.body.email }
     })
-    console.log(user)
-    if (!user || !await bcrypt.compare(req.body.password, user.password_digest)) {
+
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({ 
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user })
+        res.json({ user })                                       
     }
 })
+
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId:                    
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
+
 
 module.exports = router
 
