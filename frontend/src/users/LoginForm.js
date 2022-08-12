c9b70cec94caa5fbdbea616f0d6ef096d2321cc7
collinblank <cablank25@gmail.com>
2022-08-12 12:10:20 -0400

    created login and submit ability with bcrypt but login not working

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -17,7 +17,22 @@ function LoginForm() {
 
     async function handleSubmit(e) {
         e.preventDefault()
-       
+        const response = await fetch(`http://localhost:5000/authentication/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+
+        const data = await response.json()
+
+        if (response.status === 200) {
+            setCurrentUser(data.user)
+            history.push(`/`)
+        } else {
+            setErrorMessage(data.message)
+        }
 
     }
 
