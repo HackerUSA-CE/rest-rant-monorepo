5a617a3278f3da31c47e61c67f2fa414dcf30d37
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-09-19 19:13:57 -0400

    authentification

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,9 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 
 export const CurrentUser = createContext()
 
-function CurrentUserProvider({ children }){
+function CurrentUserProvider({ children }) {
+    useEffect(() => {
+        const getLoggedInUser = async () => {
+            try {
+                let response = await fetch('***')
+                let user = await response.json();
+                setCurrentUser(user);
+            } catch (e) {
+                debugger;
+            }
+        }
+        getLoggedInUser();
+    }, [])
 
     const [currentUser, setCurrentUser] = useState(null)
 
