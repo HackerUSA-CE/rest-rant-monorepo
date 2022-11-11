0225a9b5c3e120c96bfa4798a969f5cf0609d754
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-11-10 19:29:01 -0500

    session-authentication

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,25 @@
 // Modules and Globals
 require("dotenv").config();
+const cookieSession = require("cookie-session");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
 // Express Settings
-app.use(cors());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [process.env.SESSION_SECRET],
+    maxAge: 1000 * 60 * 60 * 24,
+  })
+);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,9 +1,22 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const CurrentUser = createContext();
 
 function CurrentUserProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
+  useEffect(function () {
+    const getLoggedInUser = async () => {
+      let response = await fetch(
+        "http://localhost:5000/authentication/profile",
+        {
+          credentials: "include",
+        }
+      );
+      let user = await response.json();
+      setCurrentUser(user);
+    };
+    getLoggedInUser();
+  }, []);
   window.setCurrentUser = setCurrentUser;
   return (
     <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
