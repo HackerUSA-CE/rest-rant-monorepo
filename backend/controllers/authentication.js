ff3725115e35dddaed868615d2d7b36b908dd6bd
Christian Salinas <ChristianSalinas1009@gmail.com>
2022-11-10 20:33:29 -0500

    set up jwt token

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,24 +1,48 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token');
 
 const { User } = db
 
-  
+
 router.post('/', async (req, res) => {
-    
+
     let user = await User.findOne({
         where: { email: req.body.email }
     })
 
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
-        res.status(404).json({ 
-            message: `Are you a hacker or did you just forget your pass? Try again :)` 
+        res.status(404).json({
+            message: `Are you a hacker or did you just forget your pass? Try again :)`
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.json({ user: user, token: result.value })
     }
 })
+
+
+router.get('/profile', async (req, res) => {
+    try {
+
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+        if (authenticationMethod == 'Bearer') {
+
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+            const { id } = result.value
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+            res.json(user)
+        }
+    } catch {
+        res.json(null)
+    }
+})
+
 
 
 module.exports = router
