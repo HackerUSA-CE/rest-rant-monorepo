eb94828af93bc8d9a885a6ab31f26422fbcd6821
Menegoth <antonioareas@me.com>
2022-11-11 16:25:41 -0800

    jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const db = require("../models");
 const bcrypt = require("bcrypt");
+const jwt = require("json-web-token");
 
 const { User } = db;
 
@@ -14,8 +15,34 @@ router.post("/", async (req, res) => {
             message: "Could not find a user with the provided username and password"
         })
     } else {
-        res.json({user})
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId });
+        res.json({user: user, token: result.value});
     }
 });
+
+router.get("/profile", async (req, res) => {
+    try {
+        //split
+        const [authenticationMethod, token] = req.headers.authorization.split(" ");
+        
+        //bearer
+        if (authenticationMethod === "Bearer") {
+
+            //decode
+            const result = await jwt.decode(process.env.JWT_SECRET, token);
+            const { id } = result.value
+
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+
+            res.json(user);
+        }
+    } catch {
+        res.json(null)
+    }
+})
 
 module.exports = router;
--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 
 export const CurrentUser = createContext()
@@ -7,6 +7,19 @@ function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null)
     window.setCurrentUser = setCurrentUser;
+
+    useEffect(() => {
+        const getLoggedInUser = async () => {
+            let response = await fetch("http://localhost:5000/authentication/profile", {
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`
+                }
+            });
+            let user = await response.json();
+            setCurrentUser(user);
+        }
+        getLoggedInUser();
+    }, [])
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
