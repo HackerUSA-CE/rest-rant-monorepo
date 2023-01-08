e24309798670ade4ae2cfd30d00e554e1934f509
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-08 13:46:18 -0600

    authentication route now checks email against password for login

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -5,7 +5,17 @@ const bcrypt = require('bcrypt')
 const { User } = db
 
 router.post('/', async (req, res) => {
-    console.log('IN HERE')
+    let user = await User.findOne({
+        where: { email: req.body.email }
+    })
+
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({
+            message: 'Could not find a user with the provided username and password'
+        })
+    } else {
+        res.status(200).json({ user })
+    }
 })
 
 module.exports = router
