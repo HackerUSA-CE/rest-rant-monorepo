156601fc9949b338ee557b7529b71f679e6af7d8
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-08 13:37:19 -0600

    added authentication route

---
+++ backend/controllers/authentication.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const db = require("../models")
+const bcrypt = require('bcrypt')
+
+const { User } = db
+
+router.post('/', async (req, res) => {
+    console.log('IN HERE')
+})
+
+module.exports = router
