3996e99fe25ef698da7ca1935bd46ab13936d8ba
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-12 21:14:42 -0600

    now only admin role can add, edit and delete places

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router"
+import { CurrentUser } from "../contexts/CurrentUser";
 import CommentCard from './CommentCard'
 import NewCommentForm from "./NewCommentForm";
 
@@ -105,6 +106,20 @@ function PlaceDetails() {
 		})
 	}
 
+	let placeActions = null
+
+	if (CurrentUser?.role === 'admin') {
+		placeActions = (
+			<>
+				<a className="btn btn-warning" onClick={editPlace}>
+					Edit
+				</a>
+				<button type="submit" className="btn btn-danger" onClick={deletePlace}>
+					Delete
+				</button>
+			</>
+		)
+	}
 
 	return (
 		<main>
@@ -132,12 +147,7 @@ function PlaceDetails() {
 						Serving {place.cuisines}.
 					</h4>
 					<br />
-					<a className="btn btn-warning" onClick={editPlace}>
-						Edit
-					</a>{` `}
-					<button type="submit" className="btn btn-danger" onClick={deletePlace}>
-						Delete
-					</button>
+					{placeActions}
 				</div>
 			</div>
 			<hr />
