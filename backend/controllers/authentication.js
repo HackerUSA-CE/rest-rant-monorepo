cc2e7e392dc0326207ab77f6841784f1db9e9671
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-12 18:27:50 -0600

    jwt auth set up

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -14,7 +15,34 @@ router.post('/', async (req, res) => {
             message: 'Could not find a user with the provided username and password'
         })
     } else {
-        res.status(200).json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.status(200).json({ user, token: result.value })
+    }
+})
+
+router.get('/profile', async (req, res) => {
+    try {
+        // split the authorization header 
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        // only handle 'Bearer' authorization for now
+        // can add more later
+        if (authenticationMethod === 'Bearer') {
+
+            // decode jwt
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            // get the logged in user's id from the payload
+            const { id } = result.value
+
+            // find the user object using their id
+            let user = await User.findOne({
+                where: { userId: id }
+            })
+            res.json(user)
+        }
+    } catch {
+        res.json(null)
     }
 })
 
--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 
 export const CurrentUser = createContext()
@@ -6,7 +6,20 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null)
-    window.setCurrentUser = setCurrentUser
+
+    useEffect(() => {
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:5000/authentication/profile', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
+
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
             {children}
