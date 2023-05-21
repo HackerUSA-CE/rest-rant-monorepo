b1b4057b522ad63b254369aa747e85ae14e4818c
Your Name <teonatskhadaia@yexample.com$ git config --global user.name teona>
2023-05-20 23:31:03 -0400

    added password_digest, using bcrypt to hash user password, fetching for login

--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -6,10 +6,13 @@ function SignUpForm() {
 	const history = useHistory()
 
 	const [user, setUser] = useState({
-		firstName: '',
-		lastName: '',
-		email: '',
-		password: ''
+		
+			firstName: "",
+			lastName: "",
+			email: "",
+			passwordDigest: ""
+		
+		
 	})
 
 	async function handleSubmit(e) {
@@ -66,6 +69,20 @@ function SignUpForm() {
 							id="email"
 							name="email"
 						/>
+					</div>
+					<div className="col-sm-6 form-group">
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							required
+							value={user.password}
+							onChange={e => setUser({ ...user, password: e.target.value })}
+							className="form-control"
+							id="password"
+							name="password"
+						/>
+
+
 					</div>
 				</div>
 				<input className="btn btn-primary" type="submit" value="Sign Up" />
