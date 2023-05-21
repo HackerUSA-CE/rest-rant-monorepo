17f4859e2ed29c634aed38a84c38179f5ce2d3d0
Teona <teonazaalishvili2@gmail.com>
2023-05-20 23:28:33 -0400

    added password_digest and using bcrypt to hash user passwords,fetching for login

---
+++ backend/migrations/20230521022507-add-user-password.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'password_digest', {
+      type: Sequelize.DataTypes.STRING
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('users', 'password_digest')
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
