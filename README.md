f8d1c05689bfa185c969825b9ca05cf28ca51e83
jssmith79 <i.am.hef@gmail.com>
2023-06-15 19:34:10 -0700

    completed assignment

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ First, you'll need a Postgres database to connect to. Follow instructions here t
 
 Next create a `.env` file inside of `backend`. It will need to contain the following environment variables (change the values for the database to match what you defined in the previous step)
 ```
-PORT=5000
+PORT=5001
 DB_USERNAME=rest_rant_user
 DB_PASSWORD=password
 DB_DATABASE=rest_rant
--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,17 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
 
+
 // Express Settings
-app.use(cors())
+app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization']
+    })
+  );
+  
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -17,6 +26,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
