be328e352f48e76b0dba9e54db9ac824b0988f02
unknown <jasmina_t05@outlook.com>
2023-06-17 17:08:24 -0700

    completed assignment Using JWTs in REST-Rant

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -30,6 +30,7 @@ async function handleSubmit(e) {
 
     if (response.status === 200) {
         setCurrentUser(data.user)
+        localStorage.setItem('token', data.token)
         history.push(`/`)
     } else {
         setErrorMessage(data.message)
