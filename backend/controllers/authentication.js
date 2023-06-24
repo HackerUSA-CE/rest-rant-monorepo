89adeba683086e8f04fca7b1f65785651b8f302b
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-06-24 14:21:19 -0700

    jsw auth

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -11,17 +12,35 @@ router.post('/', async (req, res) => {
         where: { email: req.body.email }
     })
 
-    if (!user || !await bcrypt.compare(req.body.password, uder.passwordDigest)) {
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({
             message: `could not find user`
         })
     }else {
-        res.json({user})
+        const result = await jwt.encode(process.env.JWT_SECRET, {id: user.userId})
+        res.json({user: user, token: result.value})
     }
 
     console.log(user)
 })
   
+router.get('/profile', async (req, res) => {
+    try {
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+        if (authenticationMethod == 'Bearer') {
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+            const {id} = result.value
+            let user = await User.findOne({
+            where: {
+                userId: id  
+            }
+        })
+        res.json(user)
+        } 
+    } catch {
+        res.json(null)
+    }
+})
 
 
 module.exports = router
