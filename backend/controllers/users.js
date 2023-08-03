7d2b63dd267fbdc3f299074cdf8e5be00e2af08e
VincentS972 <vincentsechi@gmail.com>
2023-08-03 10:13:23 -0400

    added password encryption

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,15 @@
 const router = require('express').Router()
 const db = require("../models")
+const bcrypt = require('bcrypt')
 
 const { User } = db
 
 router.post('/', async (req, res) => {
-    const user = await User.create(req.body)
+    let { password, ...rest } = req.body;
+    const user = await User.create({
+        ...rest,
+        passwordDigest: await bcrypt.hash(password, 10)
+    })
     res.json(user)
 })
 
