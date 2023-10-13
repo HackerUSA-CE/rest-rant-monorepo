c7ef5d58ce2030c823f5d9670e578884e81eeee4
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-10-13 17:25:12 -0400

    updated npm installation for frontend and backend to run

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
+const port = process.env.PORT || 5001;
 
 // Express Settings
 app.use(cors())
