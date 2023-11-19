40eb3abe6defcc443cfb6d5e8f4f4346a73b45e2
david vidal <davidvidal44@gmail.com>
2023-11-19 11:14:32 -0800

    pgAdmin connection

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,6 +5,7 @@ module.exports = {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
+    "port": 5432,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
