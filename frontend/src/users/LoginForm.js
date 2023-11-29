ccf0d1bb110a058a497bbb979fc6128f3a8a970b
david vidal <davidvidal44@gmail.com>
2023-11-28 23:33:58 -0800

    LoginForm code

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -17,8 +17,23 @@ function LoginForm() {
 
     async function handleSubmit(e) {
         e.preventDefault()
-       
+        const response = await fetch(`http://localhost:5000/authentication/`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
 
+        const data = await response.json()
+
+        if (response.status === 200) {
+            setCurrentUser(data.user)
+            history.push(`/`)
+        } else {
+            setErrorMessage(data.message)
+        }
     }
 
     return (
