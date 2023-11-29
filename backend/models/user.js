a75be45d579986151292ed60dd2fd1fbb8f0fa26
david vidal <davidvidal44@gmail.com>
2023-11-28 22:56:16 -0800

    sign-up-and-log-in

--- backend/models/user.js
+++ backend/models/user.js
@@ -20,7 +20,11 @@ module.exports = (sequelize, DataTypes) => {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING
-  }, {
+    passwordDigest: DataTypes.STRING
+  }, 
+  
+  
+  {
     sequelize,
     underscored: true,
     modelName: 'User',
