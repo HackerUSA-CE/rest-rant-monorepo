fcd726d31db356a2eb7b3d15fcaa65726d0da263
david vidal <davidvidal44@gmail.com>
2023-11-28 20:23:11 -0800

    session

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -3,15 +3,17 @@ import { createContext, useState } from "react";
 
 export const CurrentUser = createContext()
 
-function CurrentUserProvider({ children }){
+___
+function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
+    useEffect(() => {
 
-    return (
-        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
-            {children}
-        </CurrentUser.Provider>
-    )
-}
-
-export default CurrentUserProvider
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:5000/authentication/profile')
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
+  
