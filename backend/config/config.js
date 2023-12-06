260db3123a30e04df741c682d2cbbe420e292088
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-12-06 08:22:59 -0800

    changed localhost5000 to env variable

--- backend/config/config.js
+++ backend/config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   }
 }
--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,16 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 const app = express();
 
 // Express Settings
 app.use(cors())
+// serve static front end in production mode
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+}
+
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
