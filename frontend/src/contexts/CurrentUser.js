47f9bbc06fc0c129062e0cf6f6cfaa1935b8c86c
Kolby Kiernan <kolbykiernan@gmail.com>
2024-03-06 14:16:19 -0800

    completed in-class activity

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -5,8 +5,8 @@ export const CurrentUser = createContext()
 
 function CurrentUserProvider({ children }){
 
-    const [currentUser, setCurrentUser] = useState(null)
-
+    const [currentUser, setCurrentUser] = useState(null);
+    window.setCurrentUser = setCurrentUser;
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
             {children}
