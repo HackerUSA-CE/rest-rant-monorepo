ee7ab3fbc9aea8c51fb81431859e89800198d30c
Kolby Kiernan <kolbykiernan@gmail.com>
2024-03-06 15:59:45 -0800

    completed assignment for day 3

--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { useHistory, useParams } from "react-router"
+import { useHistory, useParams} from "react-router"
 
 function SignUpForm() {
 
@@ -55,18 +55,35 @@ function SignUpForm() {
 					</div>
 				</div>
 				<div className="row">
-					<div className="col-sm-6 form-group">
-						<label htmlFor="email">Email</label>
-						<input
-							type="email"
-							required
-							value={user.email}
-							onChange={e => setUser({ ...user, email: e.target.value })}
-							className="form-control"
-							id="email"
-							name="email"
-						/>
-					</div>
+					  
+<div className="row">
+    <div className="col-sm-6 form-group">
+        <label htmlFor="email">Email</label>
+        <input
+            type="email"
+            required
+            value={user.email}
+            onChange={e => setUser({ ...user, email: e.target.value })}
+            className="form-control"
+            id="email"
+            name="email"
+        />
+    </div>
+    <div className="col-sm-6 form-group">
+        <label htmlFor="password">Password</label>
+        <input
+            type="password"
+            required
+            value={user.password}
+            onChange={e => setUser({ ...user, password: e.target.value })}
+            className="form-control"
+            id="password"
+            name="password"
+        />
+    </div>
+</div>
+  
+
 				</div>
 				<input className="btn btn-primary" type="submit" value="Sign Up" />
 			</form>
