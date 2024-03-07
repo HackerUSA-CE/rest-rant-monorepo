139feea712eb9c6d3872c6cd4fd96fa4d0feb033
Kolby Kiernan <kolbykiernan@gmail.com>
2024-03-06 17:07:33 -0800

    completed day 4 class activity

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -16,7 +16,22 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
+        req.session.userId = user.userId
         res.json({ user })
+    }
+})
+
+router.get('/profile', async (req, res) => {
+    console.log(req.session.userId)
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
     }
 })
 
