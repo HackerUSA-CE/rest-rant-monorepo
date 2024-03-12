2707c66d089fb18ce6aca098f89f0b31648f84de
Andrew Diaz <andrewdiazn20@gmail.com>
2024-03-12 14:25:22 -0700

    backendchanges

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -3,29 +3,40 @@ const db = require("../models")
 const bcrypt = require('bcrypt')
 const jwt = require('json-web-token')           
 
+router.post('/super-important-route', async (req, res) => {
+    if(req.session.userId){
+        console.log('Do the really super important thing')
+        res.send('Done')
+    } else {
+        console.log('You are not authorized to do the super important thing')
+        res.send('Denied')
+    }
+})
+
+
+router.get('/profile', async (req, res) => {
+    console.log(req.session.userId)
+    try {
+        // let user = await User.findOne({
+        //     where: {
+        //         userId: 
+        //     }
+        // })
+        // res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
+
 router.get('/profile', async (req, res) => {
     try {
-        // Split the authorization header into [ "Bearer", "TOKEN" ]:
-        const [authenticationMethod, token] = req.headers.authorization.split(' ')
-
-        // Only handle "Bearer" authorization for now 
-        //  (we could add other authorization strategies later):
-        if (authenticationMethod == 'Bearer') {
-
-            // Decode the JWT
-            const result = await jwt.decode(process.env.JWT_SECRET, token)
-
-            // Get the logged in user's id from the payload
-            const { id } = result.value
-
-            // Find the user object using their id:
-            let user = await User.findOne({
-                where: {
-                    userId: id
-                }
-            })
-            res.json(user)
-        }
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+        res.json(user)
     } catch {
         res.json(null)
     }
